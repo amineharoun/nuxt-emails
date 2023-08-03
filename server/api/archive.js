f6d9618413668f69a44b7db0049dbd984efc3876
amine.haroun <h.amine@ouedkniss.com>
2023-08-03 08:38:02 +0100

    change sotre/datas

--- server/api/archive.js
+++ server/api/archive.js
@@ -2,35 +2,35 @@ export default defineEventHandler(() => {
   return {
     mails: [
       {
-        id: 21,
+        id: 31,
         title: "Take a class at local community center that interests you",
         read: false,
-        userId: 22,
+        userId: 33,
       },
       {
-        id: 22,
+        id: 32,
         title: "Research a topic interested in",
         read: false,
         userId: 4,
       },
       {
-        id: 23,
+        id: 33,
         title: "Plan a trip to another country",
         read: true,
         userId: 37,
       },
-      { id: 24, title: "Improve touch typing", read: false, userId: 45 },
-      { id: 25, title: "Learn Express.js", read: false, userId: 49 },
-      { id: 26, title: "Learn calligraphy", read: false, userId: 50 },
+      { id: 34, title: "Improve touch typing", read: false, userId: 45 },
+      { id: 35, title: "Learn Express.js", read: false, userId: 49 },
+      { id: 36, title: "Learn calligraphy", read: false, userId: 50 },
       {
-        id: 27,
+        id: 37,
         title: "Have a photo session with some friends",
         read: false,
         userId: 14,
       },
-      { id: 28, title: "Go to the gym", read: false, userId: 15 },
-      { id: 29, title: "Make own LEGO creation", read: false, userId: 30 },
-      { id: 30, title: "Take cat on a walk", read: false, userId: 15 },
+      { id: 38, title: "Go to the gym", read: false, userId: 15 },
+      { id: 39, title: "Make own LEGO creation", read: false, userId: 30 },
+      { id: 40, title: "Take cat on a walk", read: false, userId: 15 },
     ],
   };
 });
